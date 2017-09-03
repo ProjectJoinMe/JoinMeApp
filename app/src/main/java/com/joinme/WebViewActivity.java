760e63702112ab1888701e86e0fdf225d36b0dfc
Cleatuer <nicole.wresnik@gmail.com>
2017-09-03 17:56:35 +0200

    [general] remove intellij project files from git

--- app/src/main/java/com/joinme/WebViewActivity.java
+++ app/src/main/java/com/joinme/WebViewActivity.java
@@ -2,6 +2,9 @@ package com.joinme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -16,8 +19,17 @@ public class WebViewActivity extends AppCompatActivity {
         WebSettings webViewSettings = webView.getSettings();
         webViewSettings.setJavaScriptEnabled(true);
 
-        webView.loadUrl("http://10.0.2.2:8080"); // in emulator
-//        webView.loadUrl("http://192.168.0.117:8080"); // from app on private smartphone to machine in same network, IP has to be adapted
+        webView.setWebChromeClient(new WebChromeClient() {
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                Log.d("JoinMe", consoleMessage.message() + " -- From line "
+                        + consoleMessage.lineNumber() + " of "
+                        + consoleMessage.sourceId());
+                return true;
+            }
+        });
+
+//        webView.loadUrl("http://10.0.2.2:8080"); // in emulator
+        webView.loadUrl("http://192.168.0.117:8080"); // from app on private smartphone to machine in same network, IP has to be adapted
 //        webView.loadUrl("https://joinme.at"); // public accessible server, final version
     }
 }
